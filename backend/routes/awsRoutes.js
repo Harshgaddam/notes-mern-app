eb18db2aad48ee71a5f587d6fa2c1b22e638726d
Harshgaddam <harshvardhangaddam@gmail.com>
2024-02-11 21:32:39 +0530

    modified presigned url to displayable url

--- backend/routes/awsRoutes.js
+++ backend/routes/awsRoutes.js
@@ -24,10 +24,12 @@ const client = new S3Client({
   },
 });
 
-async function getObject(fileName) {
+async function getObject(fileName, contentType) {
   const command = new GetObjectCommand({
     Bucket: "mern-notes-app-bucket",
     Key: `uploads/${fileName}`,
+    ResponseContentType: contentType,
+    ResponseContentDisposition: "inline",
   });
   const url = await getSignedUrl(client, command);
   return url;
@@ -52,10 +54,39 @@ async function deleteObject(key) {
   return "Deleted";
 }
 
+const getContentType = (fileName) => {
+  const fileExtension = fileName.split(".").pop().toLowerCase();
+
+  const contentTypeMap = {
+    jpg: "image/jpeg",
+    jpeg: "image/jpeg",
+    png: "image/png",
+    pdf: "application/pdf",
+    bmp: "image/bmp",
+    csv: "text/csv",
+    odt: "application/vnd.oasis.opendocument.text",
+    doc: "application/msword",
+    docx: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+    gif: "image/gif",
+    htm: "text/htm",
+    html: "text/html",
+    ppt: "application/vnd.ms-powerpoint",
+    pptx: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+    tiff: "image/tiff",
+    txt: "text/plain",
+    xls: "application/vnd.ms-excel",
+    xlsx: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    mp4: "video/mp4",
+  };
+
+  return contentTypeMap[fileExtension] || "application/octet-stream";
+};
+
 router.get("/getFileURL", async (req, res) => {
   const userName = req.query.userName;
   const fileName = req.query.fileName;
-  const url = await getObject(`${userName}/${fileName}`);
+  const contentType = getContentType(fileName);
+  const url = await getObject(`${userName}/${fileName}`, contentType);
   res.json({ presignedURL: url });
 });
 
