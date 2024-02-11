a316e2e84679adad3fffe9fc172d26c89139a828
Harshgaddam <harshvardhangaddam@gmail.com>
2024-02-11 17:26:15 +0530

    changed initialization port numbers

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
   res.send("Server is ready");
 });
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.listen(port, () => {
   console.log(`Server started at http://localhost:${port}`);
